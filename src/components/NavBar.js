9018f49416852cd5cefa49095284cdf05ceadf07
Shivam Kushwaha <svm.kushwaha@gmail.com>
2024-07-04 20:29:01 +0530

    fixed navbar colors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -57,7 +57,12 @@ const NavBar = () => {
               backgroundColor: "#FFFAF0",
             }}
           >
-            <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+            <ul
+              style={{
+                backgroundColor: "#FFFAF0",
+              }}
+              className="flex flex-col font-medium mt-4 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700"
+            >
               <li>
                 <a
                   href="#features"
