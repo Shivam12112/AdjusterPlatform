83c66a498695049681b9fa8524fc376f804f10c9
Shivam Kushwaha <svm.kushwaha@gmail.com>
2024-07-04 18:14:28 +0530

    fixed alignmet issue

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -8,7 +8,7 @@ const FeatureCard = ({ icon, title, description }) => {
         display: "flex",
         flexDirection: "column",
         alignContent: "center",
-        alignItems: "center"
+        alignItems: "center",
       }}
       className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out md:flex-row md:items-start"
     >
@@ -21,7 +21,11 @@ const FeatureCard = ({ icon, title, description }) => {
           className="w-10 h-10"
         />
       </div>
-      <div>
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
         <h3 className="text-lg font-semibold mb-2">{title}</h3>
         <p className="text-gray-600">{description}</p>
       </div>
